cbaa3bb345bb64ef5088b199e65161614c07b937
kFYatek <4499762+kFYatek@users.noreply.github.com>
2026-03-14 15:58:52 +0100

    Replaced empirical tweaks from extractld.py with proper arguments

--- scripts/extractld.py
+++ scripts/extractld.py
@@ -46,9 +46,16 @@ def deqam(data):
 
 
 def depal(chroma):
+    burst_angles = numpy.angle(numpy.mean(chroma[:, :, 109:129], axis=-1))
+    burst_phases = (burst_angles[:, 167] - burst_angles[:, 166] + numpy.pi) % (
+            2.0 * numpy.pi) - numpy.pi
+    burst_mask = abs(burst_phases) < 0.4968 * numpy.pi
     palmask = numpy.ones(chroma.shape, dtype=numpy.bool)
-    palmask[1::2] = ~palmask[1::2]
+    palmask[burst_mask] = ~palmask[burst_mask]
     palmask[:, 1::2] = ~palmask[:, 1::2]
+    chroma = chroma.transpose((2, 0, 1))
+    chroma = chroma * numpy.exp(1.0j * (0.75 * numpy.pi - burst_angles))
+    chroma = chroma.transpose((1, 2, 0))
     chroma[palmask] = -numpy.imag(chroma[palmask]) - 1.0j * numpy.real(chroma[palmask])
     return chroma
 
@@ -62,54 +69,69 @@ def deinterlace(data):
 
 def _main(*args):
     parser = argparse.ArgumentParser(
-        description='Decode a still image from multiple frames of TBC information decoded using ld-decode. For now, only supports PAL CAV Laserdisc sources and includes manual tweaks tuned for the Test Card F sample found on the 1986 BBC Domesday Community South disc.')
+        description='Decode a still image from multiple frames of TBC information decoded using ld-decode. For now, only supports PAL CAV Laserdisc sources.')
     parser.add_argument('input_file', type=str,
                         help='TBC file to read data from. A matching *.json file is required too.')
     parser.add_argument('output_file', type=str,
                         help='Output file name. Will be passed through to ImageMagick.')
-    parser.add_argument('start_frame', type=int, nargs='?', default=3000,
-                        help='CAV frame number of the first frame the image to decode appears on. Default: 3000.')
-    parser.add_argument('frame_count', type=int, nargs='?', default=23,
-                        help='Number of frames the still image appears on. Must be at least 4. Default: 23.')
+    parser.add_argument('start_frame', type=int, nargs='?',
+                        help='CAV frame number of the first frame the image to decode appears on. Default is the first frame of input.')
+    parser.add_argument('frame_count', type=int, nargs='?',
+                        help='Number of frames the still image appears on. Must be at least 4. Default is all the frames.')
+    parser.add_argument('--black-level', type=float,
+                        help='Black level (on a 16-bit sample scale) to use instead of the one declared in metadata')
+    parser.add_argument('--white-level', type=float,
+                        help='White level (on a 16-bit sample scale) to use instead of the one declared in metadata')
+    parser.add_argument('--deghost', type=float, nargs=2,
+                        help='Enable deghosting. Provide shift (in samples at 4fSC) and weight values as arguments')
+    parser.add_argument('--u-scale', type=float, help='Multiplier for the U color component')
+    parser.add_argument('--v-scale', type=float, help='Multiplier for the V color component')
+    parser.add_argument('--shift', type=float,
+                        help='Number of samples (at 4fSC, possibly fractional) to shift the image by')
     args = parser.parse_args(args)
-
-    if args.frame_count < 4:
-        raise Exception('This script requires at least 4 frames to operate')
 
     with open(args.input_file + '.json') as f:
         metadata = json.load(f)
 
-    assert metadata['videoParameters']['system'] == 'PAL'
-    assert metadata['videoParameters']['fieldWidth'] == 1135
-    assert metadata['videoParameters']['fieldHeight'] == 313
+    if metadata['videoParameters']['system'] == 'PAL':
+        assert metadata['videoParameters']['fieldWidth'] == 1135
+        assert metadata['videoParameters']['fieldHeight'] == 313
+    else:
+        assert metadata['videoParameters']['system'] == 'NTSC'
+        assert metadata['videoParameters']['fieldWidth'] == 910
+        assert metadata['videoParameters']['fieldHeight'] == 263
+        raise Exception('NTSC is not yet supported')
 
     input_file_first_field = 0
 
-    fields = iter(metadata['fields'])
-    field = next(fields)
-    if not field['isFirstField']:
-        input_file_first_field -= 1
-        field = next(fields)
-    assert field['isFirstField']
-    for vbi in field['vbi']['vbiData']:
-        if vbi & 0xf00000 == 0xf00000:
-            frame_bcd = vbi ^ 0xf00000
-            frame = 0
-            multiplier = 1
-            while frame_bcd > 0:
-                frame += (frame_bcd % 16) * multiplier
-                frame_bcd //= 16
-                multiplier *= 10
-            input_file_first_field += 2 * frame
-            break
-    else:
-        raise Exception('Invalid input file')
-
     with open(args.input_file, 'rb') as f:
-        f.seek(metadata['videoParameters']['fieldWidth'] * metadata['videoParameters'][
-            'fieldHeight'] * 2 * (2 * args.start_frame - input_file_first_field))
-        data = f.read(metadata['videoParameters']['fieldWidth'] * metadata['videoParameters'][
-            'fieldHeight'] * 4 * args.frame_count)
+        if args.start_frame is not None:
+            fields = iter(metadata['fields'])
+            field = next(fields)
+            if not field['isFirstField']:
+                input_file_first_field -= 1
+                field = next(fields)
+            assert field['isFirstField']
+            for vbi in field['vbi']['vbiData']:
+                if vbi & 0xf00000 == 0xf00000:
+                    frame_bcd = vbi ^ 0xf00000
+                    frame = 0
+                    multiplier = 1
+                    while frame_bcd > 0:
+                        frame += (frame_bcd % 16) * multiplier
+                        frame_bcd //= 16
+                        multiplier *= 10
+                    input_file_first_field += 2 * frame
+                    break
+            else:
+                raise Exception('Invalid input file')
+            f.seek(metadata['videoParameters']['fieldWidth'] * metadata['videoParameters'][
+                'fieldHeight'] * 2 * (2 * args.start_frame - input_file_first_field))
+        if args.frame_count is not None:
+            data = f.read(metadata['videoParameters']['fieldWidth'] * metadata['videoParameters'][
+                'fieldHeight'] * 4 * args.frame_count)
+        else:
+            data = f.read()
 
     data = numpy.ndarray((len(data) // (
             metadata['videoParameters']['fieldWidth'] * metadata['videoParameters'][
@@ -117,12 +139,22 @@ def _main(*args):
                           metadata['videoParameters']['fieldWidth']), dtype=numpy.uint16,
                          buffer=data)
 
-    # Range that comes from metadata generated by ld-decode:
-    # input = (data - 16199.0) / 37817.0
-    # Range measured empirically for best match:
-    inp = (data - 16221.0) / 37053.0
-    # Again, empirically derived values
-    inp = deghost(inp, -4.5, 0.133333)
+    if data.shape[0] < 4:
+        raise Exception('This script requires at least 4 frames to operate')
+
+    if args.black_level is not None:
+        black_level = args.black_level
+    else:
+        black_level = metadata['videoParameters']['black16bIre']
+    if args.white_level is not None:
+        white_level = args.white_level
+    else:
+        white_level = metadata['videoParameters']['white16bIre']
+
+    inp = (data - black_level) / (white_level - black_level)
+
+    if args.deghost is not None:
+        inp = deghost(inp, *args.deghost)
 
     data = numpy.ndarray((4, inp.shape[1], inp.shape[2]))
     for i in range(4):
@@ -131,10 +163,6 @@ def _main(*args):
     luma = numpy.mean(data, axis=0)
 
     chroma = deqam(data - luma)
-    burst = numpy.mean(chroma[:, :, 109:129], axis=-1)
-    chroma = chroma.transpose((2, 0, 1))
-    chroma = chroma * numpy.exp(1.0j * (0.75 * numpy.pi - numpy.angle(burst)))
-    chroma = chroma.transpose((1, 2, 0))
     chroma = depal(chroma)
     chroma = numpy.mean(chroma, axis=0)
 
@@ -144,18 +172,21 @@ def _main(*args):
 
     fullcolor = numpy.ndarray((luma.shape[0], luma.shape[1], 3))
     fullcolor[:, :, 0] = luma
-    # 5/6 and 7/8 fixups determined empirically
-    fullcolor[:, :, 1] = 5.0 * numpy.real(chroma) / 6.0
-    fullcolor[:, :, 2] = 7.0 * numpy.imag(chroma) / 8.0
+    fullcolor[:, :, 1] = (args.u_scale if args.u_scale is not None else 1.0) * numpy.real(chroma)
+    fullcolor[:, :, 2] = (args.v_scale if args.v_scale is not None else 1.0) * numpy.imag(chroma)
 
     fullcolor = numpy.matvec(numpy.array(
         [[1.0, 0.0, 1.1402508551881414], [1.0, -0.3939307027516405, -0.5808092090310976],
          [1.0, 2.028397565922921, 0.0]]), fullcolor)
 
-    output = fullcolor[44:620, 174:1107]
-    output = common.resample(output, shift=0.4, axis=1, pad_mode='edge')
-    output = common.resample(output, 777, axis=1)
-    output = numpy.pad(output, ((0, 0), (11, 0), (0, 0)), mode='edge')
+    output = fullcolor[44:620]
+    output = numpy.pad(output, ((0, 0), (366, 365), (0, 0)), mode='edge')
+    shift = 539.21902144097223
+    if args.shift:
+        shift += args.shift
+    output = common.resample(output, shift=shift, axis=1)
+    output = common.resample(output, 1554, axis=1)
+    output = output[:, :788, :]
     outbuf = bytearray(numpy.prod(output.shape) * 2)
     outarr = numpy.ndarray(output.shape, dtype=numpy.uint16, buffer=outbuf)
     outarr[:] = numpy.round(
